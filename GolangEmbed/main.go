571b775f07461a031a671e292ad042f8ab9b0530
Ayub Subagiya <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2025-12-15 08:34:50 +0700

    Golang Embed

---
+++ GolangEmbed/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed logo.png
+var logo []byte
+
+//go:embed files/*.txt
+var path embed.FS
+
+func main() {
+	fmt.Println(version)
+
+	err := os.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dir, _ := path.ReadDir("files")
+	for _, file := range dir {
+		if !file.IsDir() {
+			fmt.Println(file.Name())
+			files, _ := path.ReadFile("files/" + file.Name())
+			fmt.Println(string(files))
+		}
+	}
+}
---
+++ GolangEmbed/test/embed_test.go
@@ -0,0 +1,59 @@
+package test
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+)
+
+//go:embed ..\version.txt
+var version string
+
+func TestEmbed(t *testing.T) {
+	fmt.Println("TestEmbed")
+	fmt.Println(version)
+}
+
+//go:embed ..\logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed files\a.txt
+//go:embed files\b.txt
+//go:embed files\c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+
+}
+
+// path matcher
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMatcher(t *testing.T) {
+	dir, _ := path.ReadDir("files")
+	for _, entry := range dir {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := files.ReadFile(entry.Name())
+			fmt.Println(string(file))
+		}
+	}
+}
