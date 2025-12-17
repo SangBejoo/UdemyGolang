fdcd348126cf1d5377fa6c5036f2078bbb805b13
Ayub Subagiya <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2025-12-17 07:41:56 +0700

    Golang - web-lanjutan-2

---
+++ GolangWeb/template_test.go
@@ -0,0 +1,89 @@
+package GolangWeb
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHtml(writer http.ResponseWriter, request *http.Request) {
+	templateText := `<html><body>{{.}}</body></html>`
+	//t, err := template.New("SIMPLE").Parse(templateText)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+	t.ExecuteTemplate(writer, "SIMPLE", "Hello HTML Template")
+}
+
+func TestSimpleHtml(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHtml(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func SimpleHtmlFile(writer *httptest.ResponseRecorder, request *http.Request) {
+	t := template.Must(template.ParseFiles("templates/simple.gohtml"))
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestSimpleHtmlFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHtmlFile(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+
+}
+
+// template directory
+func TemplateDirectory(writer http.ResponseWriter, request *http.Request) {
+	t, err := template.ParseGlob("templates/*.gohtml")
+	if err != nil {
+		panic(err)
+
+	}
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello from simple.gohtml")
+}
+
+func TestTemplateDirectory(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDirectory(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+// golang embed template
+// di rekomendasikan menggunakan golang embed untuk template agar lebih mudah di deploy
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+func TemplateEmbed(writer http.ResponseWriter, request *http.Request) {
+	t, err := template.ParseFS(templates, "templates/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello from embedded template")
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateEmbed(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
